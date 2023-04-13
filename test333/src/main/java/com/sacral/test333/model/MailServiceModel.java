0c8bee7a83d417b98fe19d429b950ccb7b87de3c
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-13 13:55:12 +0530

    initial commit

---
+++ test333/src/main/java/com/sacral/test333/model/MailServiceModel.java
@@ -0,0 +1,80 @@
+package com.sacral.test333.model;
+
+import javax.mail.internet.MimeMessage;
+
+public class MailServiceModel {
+
+    private String sender;
+    private String[] recipients;
+    private String[] cc;
+    private String[] bcc;
+    private String subject;
+    private String body;
+    private String filePath;
+    private MimeMessage mimeMessage;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String[] getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(String[] recipients) {
+        this.recipients = recipients;
+    }
+
+    public String[] getCc() {
+        return cc;
+    }
+
+    public void setCc(String[] cc) {
+        this.cc = cc;
+    }
+
+    public String[] getBcc() {
+        return bcc;
+    }
+
+    public void setBcc(String[] bcc) {
+        this.bcc = bcc;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public MimeMessage getMimeMessage() {
+        return mimeMessage;
+    }
+
+    public void setMimeMessage(MimeMessage mimeMessage) {
+        this.mimeMessage = mimeMessage;
+    }
+
+}
